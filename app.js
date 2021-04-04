4af31096056bff142c34ae49e633123ef130e8b0
Arshed github <arshedahmedmail@gmail.com>
2021-04-04 21:15:12 +0530

    add search bar

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const list = document.querySelector(".todos");
 const addTodo = document.querySelector(".add");
 const trash = document.querySelector(".delete");
+const search = document.querySelector(".search input");
 
 addTodo.addEventListener('submit', e => {
     e.preventDefault();
@@ -9,20 +10,38 @@ addTodo.addEventListener('submit', e => {
         generateTodo(todo);  // adding todos
         addTodo.reset();     // reset the input field
     }
-})
-
-// adding todos
-generateTodo = todo => {
-    list.innerHTML += `
-    <li class="list-group-item text-light d-flex justify-content-between align-items-center">
-    <span>${todo}</span>
-    <i class="fas fa-trash-alt delete"></i>
-    </li>`;
-}
+});
 
 // delete todos
 list.addEventListener('click', e => {
     if (e.target.classList.contains('delete')) {
         e.target.parentElement.remove();
     }
-})
+});
+
+// search bar
+search.addEventListener('keyup', () => {
+    const key = search.value.trim();
+    filterTodos(key);
+    
+});
+    
+// adding todos
+const generateTodo = todo => {
+    list.innerHTML += `
+        <li class="list-group-item text-light d-flex justify-content-between align-items-center">
+        <span>${todo}</span>
+        <i class="fas fa-trash-alt delete"></i>
+        </li>`;
+};
+
+const filterTodos = key => {
+    Array.from(list.children)
+        .filter(todo => !todo.textContent.includes(key))
+        .forEach(todo => todo.classList.add("filtered"));
+
+    Array.from(list.children)
+        .filter(todo => todo.textContent.includes(key))
+        .forEach(todo => todo.classList.remove("filtered"));
+}
+    
