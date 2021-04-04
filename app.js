be1731b26296732fdd4d6332964195af92e7c8a8
Arshed github <arshedahmedmail@gmail.com>
2021-04-04 20:37:17 +0530

    add todos

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+const list = document.querySelector(".todos");
+const addTodo = document.querySelector(".add");
 const trash = document.querySelector(".delete");
+
+addTodo.addEventListener('submit', e => {
+    e.preventDefault();
+    const todo = addTodo.add.value.trim();
+    if (todo.length) {
+        generateTodo(todo);  // adding todos
+        addTodo.reset();     // reset the input field
+    }
+})
+
+// adding todos
+generateTodo = todo => {
+    list.innerHTML += `
+    <li class="list-group-item text-light d-flex justify-content-between align-items-center">
+    <span>${todo}</span>
+    <i class="fas fa-trash-alt delete"></i>
+    </li>`;
+}
+
+// delete todos
+list.addEventListener('click', e => {
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
+})
